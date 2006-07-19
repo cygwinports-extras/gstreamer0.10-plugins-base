a05fd9426363267e3191f4d2fba848f70ec775fe
yselkowitz <yselkowitz@e127d73d-b18e-4ae1-a744-a22e6a3ccd30>
2006-07-19 01:59:41 +0000

    Rearrange for parallel versions of GStreamer.
    git-svn-id: https://cygwin-ports.svn.sourceforge.net/svnroot/cygwin-ports/ports/trunk@147 e127d73d-b18e-4ae1-a744-a22e6a3ccd30

---
+++ postinstall.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+prefix=/usr
+bindir=${prefix}/bin
+slot=0.10
+rank=10
+
+/usr/sbin/update-alternatives \
+	--install ${bindir}/gst-visualise  gst-visualise  ${bindir}/gst-visualise-${slot} ${rank}
---
+++ preremove.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+prefix=/usr
+bindir=${prefix}/bin
+
+/usr/sbin/update-alternatives --remove gst-visualise ${bindir}/gst-visualise-0.10
