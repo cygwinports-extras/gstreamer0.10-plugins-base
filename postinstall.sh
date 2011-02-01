1ff9f0fbc01fdbc4e36ed6daff25d516755b5f29
Yaakov Selkowitz <yselkowitz@users.sourceforge.net>
2011-02-01 05:02:00 -0600

    gstreamer0.10-plugins-base 0.10.32

--- postinstall.sh
+++ postinstall.sh
@@ -5,4 +5,5 @@ slot=0.10
 rank=10
 
 /usr/sbin/update-alternatives \
-	--install ${bindir}/gst-visualise  gst-visualise  ${bindir}/gst-visualise-${slot} ${rank}
+	--install ${bindir}/gst-visualise  gst-visualise  ${bindir}/gst-visualise-${slot} ${rank} \
+	--slave   ${bindir}/gst-discoverer gst-discoverer ${bindir}/gst-discoverer-${slot}.exe
