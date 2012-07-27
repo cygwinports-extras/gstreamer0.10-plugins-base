60347708a0bfa2a7a9d08c4f7fe938853b31c86e
Yaakov Selkowitz <yselkowitz@users.sourceforge.net>
2012-07-27 05:29:43 -0500

    Rebuilt for ITP

--- postinstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-prefix=/usr
-bindir=${prefix}/bin
-slot=0.10
-rank=10
-
-/usr/sbin/update-alternatives \
-	--install ${bindir}/gst-visualise  gst-visualise  ${bindir}/gst-visualise-${slot} ${rank} \
-	--slave   ${bindir}/gst-discoverer gst-discoverer ${bindir}/gst-discoverer-${slot}.exe
--- preremove.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-prefix=/usr
-bindir=${prefix}/bin
-
-/usr/sbin/update-alternatives --remove gst-visualise ${bindir}/gst-visualise-0.10
